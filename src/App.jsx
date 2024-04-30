c0f6db92793d9f55b301bf7de3ddedf08e43b2ee
Deonis Peretyagin <karudo@gmail.com>
2024-04-30 10:17:22 +0400

    learn notes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css';
-import React from 'react'
-import { useState, useContext, createContext } from "react";
+import React from 'react';
+import { FindScaleApp } from './FindScale';
+import './TrainNotes.css';
+import { StringRow } from './FindScale';
 
-const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+export const notes = ["C", "D", "E", "F", "G", "A", "B"];
 
-const notesOtherNames = {
+export const notesOtherNames = {
   "C": "До",
   "C#": "До#",
   "D": "Ре",
@@ -19,183 +20,82 @@ const notesOtherNames = {
   "B": "Си"
 }
 
-const scales = [
-  {
-    name: "Accord Major",
-    scale: [4, 3]
-  },
-  {
-    name: "Accord Minor",
-    scale: [3, 4]
-  },
-  {
-    name: "Accord Dim",
-    scale: [3, 3, 3]
-  },
-  {
-    name: "Scale Major",
-    scale: [2, 2, 1, 2, 2, 2, 1]
-  },
-  {
-    name: "Scale Minor",
-    scale: [2, 1, 2, 2, 1, 2, 2]
-  },
-  {
-    name: "Penta Major",
-    scale: [2, 2, 3, 2]
-  },
-  {
-    name: "Penta Minor",
-    scale: [3, 2, 2, 3]
-  }
-];
-
-const Cont1 = createContext();
-const Cont2 = createContext();
-
-function findScale(note, scale) {
-  let lastIdx = notes.findIndex((n) => n === note);
-  const indexes = [lastIdx];
-  for (const s of scale) {
-    lastIdx += s;
-    indexes.push(lastIdx % 12);
-  }
-  return indexes.map((i) => notes[i]);
+function randomNote() {
+  return notes[Math.floor(Math.random() * notes.length)];
 }
 
-function Square({ note, children }) {
-  return (
-      <div className="square">
-        <div className="note">{note}</div>
-        {children}
-      </div>
-  );
+function wait(s) {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
 }
 
-function StringRow({ note }) {
-  const thisScale1 = useContext(Cont1);
-  const thisScale2 = useContext(Cont2);
-
-  const [rowNotes] = useState(() => {
-    const index = notes.findIndex((n) => n === note);
-    const rowNotes = [...notes.slice(index), ...notes.slice(0, index)]
-    return rowNotes;
-  });
+export function TrainNotesApp() {
+  const [note, setNote] = React.useState(randomNote());
+  const [altName, setAltName] = React.useState(false);
+  const [show, setShow] = React.useState('note');
+  React.useEffect(() => {
+    let isRunning = true;
+    (async () => {
+      while (isRunning) {
+        const note = randomNote();
+        setNote(note);
+        setAltName(Math.random() < 0.1);
+        setShow('note');
+        await wait(5);
+        setShow('fretboard');
+        await wait(5);
+      }
+    })();
+    return () => {
+      isRunning = false;
+    };
+  }, []);
   return (
-      <div className="row">
-        {rowNotes.map((curNote) => {
-          const position1 = thisScale1.findIndex((n) => n === curNote);
-          const position2 = thisScale2.findIndex((n) => n === curNote);
-          return (
-            <Square note={curNote} key={curNote}>
-              {position1 >= 0 && <div className="position1">{position1 + 1}</div>}
-              {position2 >= 0 && <div className="position2">{position2 + 1}</div>}
-            </Square>
-          );
-        })}
+    <div>
+      <div style={ { fontSize: 108, display: 'flex', justifyContent: 'center' } }>
+        { altName ? notesOtherNames[note] : note }
       </div>
+      <div>
+        { show === 'fretboard' && (
+          <div>
+            <StringRow note="G" scale1={ [note] } scale2={ [] }/>
+            <StringRow note="D" scale1={ [note] } scale2={ [] }/>
+            <StringRow note="A" scale1={ [note] } scale2={ [] }/>
+            <StringRow note="E" scale1={ [note] } scale2={ [] }/>
+          </div>
+        ) }
+      </div>
+    </div>
   );
 }
 
 export default function App() {
-  const [strings, setStrings] = useState(4);
-  const [curNote1, setCurNote1] = useState("C");
-  const [curNote2, setCurNote2] = useState("");
-  const [curScale1, setCurScale1] = useState(0);
-  const [curScale2, setCurScale2] = useState(0);
+  const [app, setApp] = React.useState("TrainNotesApp");
   return (
-      <div className="App">
-        <div style={{ display: 'flex', marginBottom: '24px' }}>
-          <div style={{marginRight: '30px'}}>
-            <select value={curNote1} onChange={(e) => setCurNote1(e.target.value)}>
-              {notes.map((n) => (
-                <option value={n} key={n}>
-                  {n}
-                </option>
-              ))}
-            </select>
-            {scales.map((sc) => (
-              <div key={sc.name}>
-                {sc.name}: {findScale(curNote1, sc.scale).join(", ")}
-              </div>
-            ))}
-            <select
-              value={curScale1}
-              onChange={(e) => setCurScale1(+e.target.value)}
-              style={{ marginTop: '12px' }}
-            >
-              {scales.map((s, index) => (
-                <option value={index} key={index}>
-                  {s.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <select value={curNote2} onChange={(e) => setCurNote2(e.target.value)}>
-              <option value="">Skip</option>
-              {notes.map((n) => (
-                <option value={n} key={n}>
-                  {n}
-                </option>
-              ))}
-            </select>
-            {curNote2 && (
-                <>
-                  {scales.map((sc) => (
-                    <div key={sc.name}>
-                      {sc.name}: {findScale(curNote2, sc.scale).join(", ")}
-                    </div>
-                  ))}
-                  <select
-                    value={curScale2}
-                    onChange={(e) => setCurScale2(+e.target.value)}
-                    style={{ marginTop: '12px' }}
-                  >
-                    {scales.map((s, index) => (
-                      <option value={index} key={index}>
-                        {s.name}
-                      </option>
-                    ))}
-                  </select>
-                </>
-            )}
-          </div>
-        </div>
-
-        <Cont2.Provider
-          value={curNote2 ? findScale(curNote2, scales[curScale2].scale) : []}
-        >
-          <Cont1.Provider value={findScale(curNote1, scales[curScale1].scale)}>
-            <StringRow note="G" />
-            <StringRow note="D" />
-            <StringRow note="A" />
-            <StringRow note="E" />
-            {strings === 5 && <StringRow note="B" />}
-          </Cont1.Provider>
-        </Cont2.Provider>
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="strings"
-              value="4"
-              checked={strings === 4}
-              onChange={() => setStrings(4)}
-            />
-            4 strings
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="strings"
-              value="5"
-              checked={strings === 5}
-              onChange={() => setStrings(5)}
-            />
-            5 strings
-          </label>
-        </div>
+    <div>
+      <div style={ { padding: '12px 0', fontSize: 20 }}>
+        <label>
+          <input
+            type="radio"
+            name="app"
+            value="TrainNotesApp"
+            checked={app === 'TrainNotesApp'}
+            onChange={() => setApp('TrainNotesApp')}
+          />
+          TrainNotesApp
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="app"
+            value="FindScaleApp"
+            checked={app === 'FindScaleApp'}
+            onChange={() => setApp('FindScaleApp')}
+          />
+          FindScaleApp
+        </label>
       </div>
+      {app === 'TrainNotesApp' && <TrainNotesApp/>}
+      {app === 'FindScaleApp' && <FindScaleApp/>}
+    </div>
   );
 }
---
+++ src/FindScale.jsx
@@ -0,0 +1,186 @@
+import './FindScale.css';
+import React from 'react'
+import { useState } from "react";
+
+export const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+export const notesOtherNames = {
+  "C": "До",
+  "C#": "До#",
+  "D": "Ре",
+  "D#": "Ре#",
+  "E": "Ми",
+  "F": "Фа",
+  "F#": "Фа#",
+  "G": "Соль",
+  "G#": "Соль#",
+  "A": "Ля",
+  "A#": "Ля#",
+  "B": "Си"
+}
+
+const scales = [
+  {
+    name: "Accord Major",
+    scale: [4, 3]
+  },
+  {
+    name: "Accord Minor",
+    scale: [3, 4]
+  },
+  {
+    name: "Accord Dim",
+    scale: [3, 3, 3]
+  },
+  {
+    name: "Scale Major",
+    scale: [2, 2, 1, 2, 2, 2, 1]
+  },
+  {
+    name: "Scale Minor",
+    scale: [2, 1, 2, 2, 1, 2, 2]
+  },
+  {
+    name: "Penta Major",
+    scale: [2, 2, 3, 2]
+  },
+  {
+    name: "Penta Minor",
+    scale: [3, 2, 2, 3]
+  }
+];
+
+function findScale(note, scale) {
+  let lastIdx = notes.findIndex((n) => n === note);
+  const indexes = [lastIdx];
+  for (const s of scale) {
+    lastIdx += s;
+    indexes.push(lastIdx % 12);
+  }
+  return indexes.map((i) => notes[i]);
+}
+
+function Square({ note, children }) {
+  return (
+    <div className="square">
+      <div className="note">{note}</div>
+      {children}
+    </div>
+  );
+}
+
+export function StringRow({ note, scale1, scale2 }) {
+  const [rowNotes] = useState(() => {
+    const index = notes.findIndex((n) => n === note);
+    const rowNotes = [...notes.slice(index), ...notes.slice(0, index)]
+    return rowNotes;
+  });
+  return (
+    <div className="row">
+      {rowNotes.map((curNote) => {
+        const position1 = scale1.findIndex((n) => n === curNote);
+        const position2 = scale2.findIndex((n) => n === curNote);
+        return (
+          <Square note={curNote} key={curNote}>
+            {position1 >= 0 && <div className="position1">{position1 + 1}</div>}
+            {position2 >= 0 && <div className="position2">{position2 + 1}</div>}
+          </Square>
+        );
+      })}
+    </div>
+  );
+}
+
+function SelectScale({ note, onChangeNote, scale, onChangeScale }) {
+  return (
+    <div className="selectScale">
+      <div>
+        <select value={ note } onChange={ (e) => onChangeNote(e.target.value) }>
+          <option value="">Skip</option>
+          { notes.map((n) => (
+            <option value={ n } key={ n }>
+              { n }
+            </option>
+          )) }
+        </select>
+      </div>
+      <div>
+      {scales.map((sc) => (
+          <div key={ sc.name }>
+            {sc.name}: {note && findScale(note, sc.scale).join(", ")}
+          </div>
+        ))}
+      </div>
+      <div>
+        <select
+          value={scale}
+          onChange={(e) => onChangeScale(+e.target.value)}
+        >
+          {scales.map((s, index) => (
+            <option value={index} key={index}>
+              {s.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export function FindScaleApp() {
+  const [strings, setStrings] = useState(4);
+  const [curNote1, setCurNote1] = useState("C");
+  const [curNote2, setCurNote2] = useState("");
+  const [curScale1, setCurScale1] = useState(0);
+  const [curScale2, setCurScale2] = useState(0);
+  const scale1 = curNote1 ? findScale(curNote1, scales[curScale1].scale) : [];
+  const scale2 = curNote2 ? findScale(curNote2, scales[curScale2].scale) : [];
+  return (
+    <div className="App">
+      <div style={{ display: 'grid', gridAutoFlow: 'column', justifyContent: 'start', gap: 32, marginBottom: '24px' }}>
+        <SelectScale
+          note={curNote1}
+          onChangeNote={setCurNote1}
+          scale={curScale1}
+          onChangeScale={setCurScale1}
+        />
+        <SelectScale
+          note={curNote2}
+          onChangeNote={setCurNote2}
+          scale={curScale2}
+          onChangeScale={setCurScale2}
+        />
+      </div>
+
+      <div>
+        <StringRow note="G" scale1={scale1} scale2={scale2} />
+        <StringRow note="D" scale1={scale1} scale2={scale2} />
+        <StringRow note="A" scale1={scale1} scale2={scale2} />
+        <StringRow note="E" scale1={scale1} scale2={scale2} />
+        {strings === 5 && <StringRow note="B" />}
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="strings"
+            value="4"
+            checked={strings === 4}
+            onChange={() => setStrings(4)}
+          />
+          4 strings
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="strings"
+            value="5"
+            checked={strings === 5}
+            onChange={() => setStrings(5)}
+          />
+          5 strings
+        </label>
+      </div>
+    </div>
+  );
+}
